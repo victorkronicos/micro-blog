45ee445700ace09f75e26ca5e7c96f4d81c80822
Victor Henrique <victor_henrique@live.com>
2022-08-15 17:17:56 -0300

    up: posts blog

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
-/* eslint-disable import/no-anonymous-default-export */
 import React from "react";
 import PostCreate from "./PostCreate";
+import PostList from "./PostList";
 
-export default () => {
+const App = () => {
   return (
     <div className="container">
+      <h1>Create Post</h1>
       <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
     </div>
   );
 };
+export default App;
