607a9c3e2e19971d4d90b2d6bc539224a4a0e9ea
Victor Henrique <victor_henrique@live.com>
2022-08-15 17:14:23 -0300

    add cors

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
+/* eslint-disable import/no-anonymous-default-export */
 import React from "react";
+import PostCreate from "./PostCreate";
 
-const Main = () => {
-  return <div>Blog app</div>;
+export default () => {
+  return (
+    <div className="container">
+      <PostCreate />
+    </div>
+  );
 };
-
-export default Main;
